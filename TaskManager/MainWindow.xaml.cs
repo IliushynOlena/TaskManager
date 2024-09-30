6ad0ba134427e5adbcfc0ac4342edb472b2dae12
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-09-30 17:44:52 +0300

    Add project files.

---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+           MessageBox.Show(  (timeCB.SelectedItem).ToString() );
+           MessageBox.Show(check.Content.ToString());
+
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process prToKill = (grid.SelectedItem as Process);
+            MessageBox.Show(prToKill.ProcessName);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(nameProcess.Text);
+            
+        }
+    }
+}
